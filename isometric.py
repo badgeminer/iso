7eb66c33a585f5ba064154c3c3ebc3edaf8b8f32
Replit user <>
2023-04-19 01:40:03 +0000

    fdsaf

--- isometric.py
+++ isometric.py
@@ -3,28 +3,34 @@ import pygame, sys, time, random
 from pygame.locals import *
 pygame.init()
 pygame.display.set_caption('game base')
-screen = pygame.display.set_mode((1000, 900))
-display = pygame.Surface((300, 300))
+scr = pygame.display.set_mode((1300, 900))
+#display = pygame.Surface((650, 450))
+display = pygame.Surface((1300, 900))
 
-grass_img = pygame.image.load('wall.png').convert()
+grass_img = pygame.image.load('wall.png').convert_alpha()
 grass_img.set_colorkey((0, 0, 0))
+floor_img = pygame.image.load('floors.png').convert_alpha()
+floor_img.set_colorkey((0, 0, 0))
+sel_img = pygame.image.load('sel.png').convert_alpha()
+sel_img.set_colorkey((0, 0, 0))
 
-f = open('map.txt')
+f = open('mp.txt')
 map_data = [[int(c) for c in row] for row in f.read().split('\n')]
 f.close()
-
+cx,cy = 0,0
 while True:
     display.fill((0,0,0))
+    #scr.fill((0,0,0))
 
     for y, row in enumerate(map_data):
         for x, tile in enumerate(row):
+            X = x +15
+            Y = y
             if tile:
-                X = x +10
-                Y = y
-                #pygame.draw.rect(display, (255, 255, 255), pygame.Rect(x * 10, y * 10, 10, 10), 1)
                 display.blit(grass_img, ((X-Y)*16,(X+Y)*8))
-                #if random.randint(0, 1):
-                #    display.blit(grass_img, (150 + x * 10 - y * 10, 100 + x * 5 + y * 5 - 14))
+            else:
+                display.blit(floor_img, (((X-Y)*16),((X+Y)*8) +16))
+              
 
     for event in pygame.event.get():
         if event.type == QUIT:
@@ -35,6 +41,6 @@ while True:
                 pygame.quit()
                 sys.exit()
 
-    screen.blit(pygame.transform.scale(display, screen.get_size()), (0, 0))
+    scr.blit(pygame.transform.scale(display, scr.get_size()), (0, 0))
     pygame.display.update()
     time.sleep(1)
